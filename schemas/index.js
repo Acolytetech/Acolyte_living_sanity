c949cf5940a6e813b24d482b7ea47cf0b9872af3
acolytetech <slwaniya03@gmail.com>
2025-11-11 18:29:47 +0530

    final

--- schemas/index.js
+++ schemas/index.js
@@ -2,4 +2,5 @@ import {documentSchemas} from './documents'
 import {componentSchemas} from './components'
 import {pluginSchemas} from './plugins'
 
+
 export const schemaTypes = [...documentSchemas, ...componentSchemas, ...pluginSchemas]
